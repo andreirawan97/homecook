e438f2db45c35435770eb7c0c09c8ca078e297ab
Andre Irawan <andreirawan97@gmail.com>
2021-08-31 16:15:35 +0700

    Recipe detail enhancements

--- src/scenes/RecipeDetailPage.tsx
+++ src/scenes/RecipeDetailPage.tsx
@@ -27,12 +27,15 @@ export default function RecipeDetail() {
   const [isRecipeSaved, setRecipeSaved] = useState(
     isInSavedRecipes(String(recipeId))
   );
+  const [loadingRecipeDetail, setLoadingRecipeDetail] = useState(true);
 
   const getRecipeDetail = () => {
     axios.get(URLBuilder("recipeDetail", {}, "", recipeId)).then((res) => {
       const data = res.data as Recipe;
       setRecipeData(data);
       console.log(data);
+
+      setLoadingRecipeDetail(false);
     });
   };
 
@@ -64,85 +67,110 @@ export default function RecipeDetail() {
           <span className="text-xl">Back</span>
         </div>
 
-        <div className="z-10" onClick={onClickSaveRecipe}>
-          {isRecipeSaved ? (
-            <BookmarkFilledIcon className="h-9 w-9 text-gray-500" />
-          ) : (
-            <BookmarkIcon className="h-9 w-9 text-gray-500" />
-          )}
-        </div>
+        {!loadingRecipeDetail ? (
+          <div className="z-10" onClick={onClickSaveRecipe}>
+            {isRecipeSaved ? (
+              <BookmarkFilledIcon className="h-9 w-9 text-gray-500" />
+            ) : (
+              <BookmarkIcon className="h-9 w-9 text-gray-500" />
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
-      <div className="flex flex-row md:flex-col items-center justify-center mb-6">
-        <img
-          className="rounded-lg mr-6 md:mb-3 w-72 h-40"
-          alt=""
-          src={recipeData?.image}
-        />
 
+      {!loadingRecipeDetail ? (
         <div>
-          <h2 className="font-bold">{recipeData?.title}</h2>
+          <div className="flex flex-row md:flex-col items-center justify-center mb-6">
+            <div className="flex flex-row max-w-2xl">
+              <img
+                className="rounded-lg mr-6 md:mb-3 w-72 h-40"
+                alt=""
+                src={recipeData?.image}
+              />
 
-          <span className="text-gray-600 mr-2">
-            Serving:{" "}
-            <span className="text-black font-bold">{recipeData?.servings}</span>
-          </span>
-          <br />
+              <div>
+                <h2 className="font-bold">{recipeData?.title}</h2>
 
-          <span className="text-gray-600 mr-2">
-            Cooking Time:{" "}
-            <span className="text-black font-bold">
-              {recipeData?.readyInMinutes} minutes
-            </span>
-          </span>
-          <br />
+                <span className="text-gray-600 mr-2">
+                  Serving:{" "}
+                  <span className="text-black font-bold">
+                    {recipeData?.servings}
+                  </span>
+                </span>
+                <br />
 
-          <span className="text-gray-600 mr-2">
-            Diets:{" "}
-            <span className="text-black font-bold">
-              {arrayToString(recipeData?.diets)}
-            </span>
-          </span>
-          <br />
+                <span className="text-gray-600 mr-2">
+                  Cooking Time:{" "}
+                  <span className="text-black font-bold">
+                    {recipeData?.readyInMinutes} minutes
+                  </span>
+                </span>
+                <br />
 
-          <span className="text-gray-600 mr-2">
-            Dish types:{" "}
-            <span className="text-black font-bold">
-              {arrayToString(recipeData?.dishTypes)}
-            </span>
-          </span>
-        </div>
-      </div>
+                <span className="text-gray-600 mr-2">
+                  Diets:{" "}
+                  <span className="text-black font-bold">
+                    {arrayToString(recipeData?.diets)}
+                  </span>
+                </span>
+                <br />
 
-      <div className="pt-3 pl-3 pr-3 pb-1 bg-white rounded-2xl shadow">
-        <Collapsible
-          title="Summary"
-          content={
-            <div className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              {htmlParser(recipeData?.summary ?? "")}
+                <span className="text-gray-600 mr-2">
+                  Dish types:{" "}
+                  <span className="text-black font-bold">
+                    {arrayToString(recipeData?.dishTypes)}
+                  </span>
+                </span>
+              </div>
             </div>
-          }
-        />
+          </div>
 
-        <Collapsible
-          title="Ingredients"
-          content={recipeData?.extendedIngredients.map((ingredient) => (
-            <div className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              <Checkbox caption={ingredient.original} />
-            </div>
-          ))}
-        />
+          <div className="pt-3 pl-3 pr-3 pb-1 bg-white rounded-2xl shadow">
+            <Collapsible
+              title="Summary"
+              content={
+                <div className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                  {htmlParser(recipeData?.summary ?? "")}
+                </div>
+              }
+            />
 
-        <Collapsible
-          title="Instructions"
-          content={
-            <div className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              {recipeData?.analyzedInstructions[0].steps.map((instruction) => (
-                <p className="mb-2">{instruction.step}</p>
+            <Collapsible
+              title="Ingredients"
+              content={recipeData?.extendedIngredients.map((ingredient) => (
+                <div className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                  <Checkbox caption={ingredient.original} />
+                </div>
               ))}
-            </div>
-          }
-        />
-      </div>
+            />
+
+            <Collapsible
+              title="Instructions"
+              content={
+                <div className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                  {recipeData && recipeData.analyzedInstructions.length ? (
+                    recipeData?.analyzedInstructions[0].steps.map(
+                      (instruction, i) => (
+                        <p className="mb-2">{`${i + 1}. ${
+                          instruction.step
+                        }`}</p>
+                      )
+                    )
+                  ) : (
+                    <span>
+                      This is weird but this recipe doesn't have an Instructions
+                    </span>
+                  )}
+                </div>
+              }
+            />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
